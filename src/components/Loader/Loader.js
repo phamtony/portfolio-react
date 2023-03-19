eba382bda2a0abc620241dc8b0990001acbf50fc
Tony Pham <mrphamtony@gmail.com>
2023-03-18 23:59:39 -0700

    changed loader page

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 const Loader = () => {
     const messages = [
-        'I\'m using the free plan for heroku',
-        'It takes a little bit of time loading the API',
-        'It only happens when the app is inactive',
-        'If you refresh after the portfolio loads,',
-        'it\'ll load pretty quick from here on out',
+        'I\'m using the free plan',
+        'It\'s taking a bit of time loading the API',
         'It\'ll be done soon, thanks for visiting!'
     ];
 
@@ -27,7 +24,7 @@ const Loader = () => {
         <div className={"d-flex justify-content-center d-flex align-items-center"} style={{height: "100vh"}}>
             <div className={"text-center"}>
                 <div className="spinner-border" style={{width: "7rem", height: "7rem"}} role="status"></div>
-                <h3 className={"mt-5"} id={"loaderMsg"}>Heroku is taking a little bit of time...</h3>
+                <h3 className={"mt-5"} id={"loaderMsg"}>The site make take a bit of time...</h3>
             </div>
         </div>
     );
