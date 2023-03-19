1a436b224f4c9d234f9901457f9cb2946604540e
Tony Pham <mrphamtony@gmail.com>
2023-03-18 23:57:45 -0700

    changed api key

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@ export const imageAWSlink = 'https://portfolio--api.s3-us-west-1.amazonaws.com';
 
 // API Portfolio Service
 export const portfolioAPI = 'https://portfolio-api.com/json';
-export const apiKey = 'sJfvLXK6yNeszieYlwGotBrf';
+export const apiKey = 'lSNFA7gewSk96hCZ9tGplIwf';
